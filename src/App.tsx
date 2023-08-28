42e50cbc312e40ec8863b4a196e475c642bab9a1
Serhii Trachuk <s.trachuk@apiko.com>
2023-08-28 11:53:34 +0300

    Add base implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
-function App() {
-  return <h1>Hello world</h1>;
+import { useEffect, useState } from "react";
+
+interface ClockProps {
+  onChange: (value: number) => void;
 }
+
+const Clock = ({ onChange }: ClockProps) => {
+  console.log("Clock render");
+
+  useEffect(() => {
+    let tick = 0;
+
+    const interval = setInterval(() => {
+      tick += 1;
+      onChange(tick);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [onChange]);
+
+  return <h1>Clock is ticking</h1>;
+};
+
+interface ClockWatcherProps {
+  value: number;
+}
+
+const ClockWatcher = ({ value }: ClockWatcherProps) => {
+  console.log("ClockWatcher render");
+
+  return <h1>{value}</h1>;
+};
+
+const App = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <Clock onChange={setValue} />
+      <ClockWatcher value={value} />
+    </div>
+  );
+};
 
 export default App;
